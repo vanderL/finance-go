daae0c35217449a1bb81c27aedfef8cba433c68c
Vander Lima <vnd.vander@gmail.com>
2022-10-25 14:56:20 -0300

    feat: Integration React Hook Form

---
+++ src/Components/Forms/InputForm/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { TextInputProps } from 'react-native';
+import { Control, Controller } from 'react-hook-form';
+import { Input } from '../Input';
+
+import * as Sc from './styles';
+
+interface Props extends TextInputProps {
+  control: Control;
+  name: string;
+}
+
+export function InputForm({
+  control,
+  name,
+  ...rest
+}: Props) {
+
+  return (
+    <Sc.Container>
+      <Controller
+        control={control}
+        render={({
+          field: { onChange, value }
+        }) => (
+          <Input
+            onChangeText={onChange}
+            value={value}
+            {...rest}
+          />
+        )}
+        name={name}
+      />
+
+    </Sc.Container>
+  );
+}
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-
+import { useForm } from 'react-hook-form';
 import { Modal } from 'react-native';
 
 import { Button } from "../../Components/Forms/Button";
 import { CategorySelectButton } from "../../Components/Forms/CategorySelectButton";
+import { InputForm } from "../../Components/Forms/InputForm";
 import { Input } from "../../Components/Forms/Input";
 import { TransactionTypeButton } from "../../Components/Forms/TransactionTypeButton";
 
@@ -11,13 +12,21 @@ import { CategorySelect } from '../CategorySelect';
 
 import * as Sc from './styles';
 
+interface FormData {
+  name: string;
+  amount: string;
+}
+
 export function Register() {
   const [transactionType, setTransactionType] = useState('');
-  const [categoryModalOpen, setCategoryModalOpen] = useState(false)
+  const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+
   const [category, setCategory] = useState({
     key: 'category',
     name: 'Categoria',
   });
+
+  const { control, handleSubmit } = useForm();
 
   function handleTransactionsTypesSelect(type: 'up' | 'down') {
     setTransactionType(type)
@@ -31,6 +40,16 @@ export function Register() {
     setCategoryModalOpen(true)
   }
 
+  function handleRegister(form: FormData) {
+    const data = {
+      name: form.name,
+      amount: form.amount,
+      transactionType,
+      category: category.key
+    }
+    console.log(data)
+  }
+
   return (
     <Sc.Container>
       <Sc.Header>
@@ -41,10 +60,14 @@ export function Register() {
 
       <Sc.Form>
         <Sc.Fields>
-          <Input
+          <InputForm
+            name="name"
+            control={control}
             placeholder="Nome"
           />
-          <Input
+          <InputForm
+            name="amount"
+            control={control}
             placeholder="Preço"
           />
 
@@ -71,6 +94,7 @@ export function Register() {
 
         <Button
           title="Enviar"
+          onPress={handleSubmit(handleRegister)}
         />
       </Sc.Form>
 
