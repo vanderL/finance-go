644c5f0851e9221e9db2d3b6d32d6f2e3b725f72
Vander Lima <vnd.vander@gmail.com>
2022-10-26 16:49:43 -0300

    feat: Using Tab Navigation

--- App.tsx
+++ App.tsx
@@ -6,13 +6,13 @@ import {
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold
-} from '@expo-google-fonts/poppins'
+} from '@expo-google-fonts/poppins';
 
-import theme from './src/global/styles/theme'
+import theme from './src/global/styles/theme';
 
-// import { Dashboard } from './src/screens/Dashboard';
-import { Register } from './src/screens/Register';
-// import { CategorySelect } from './src/screens/CategorySelect';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,13 +22,13 @@ export default function App() {
   });
 
   if (!fontsLoaded) {
-    return null
+    return null;
   }
   return (
     <ThemeProvider theme={theme}>
-      {/* <Dashboard /> */}
-      <Register />
-      {/* <CategorySelect /> */}
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
